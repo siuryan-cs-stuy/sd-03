7be833c3444a797c8e33e2c238904f796bed7409
Ryan Siu <rsiu@stuy.edu>
2017-09-15 11:01:31 -0400

    First working version

--- occupation.py
+++ occupation.py
@@ -10,10 +10,10 @@ def generate_occupations():
     for line in f:
         line = line.strip()
         if line[0] == '"':
-            occupation = line.strip('"').split('"')
-            occupation[1] = occupation[1].strip(',')
-        if not occupation[1].isalpha():
-            occ_dict[occupation[0]] = float(occupation[1])
+            line = line.strip('"').split('"')
+            line[1] = line[1].strip(',')
+        if not line[1].isalpha():
+            occ_dict[line[0]] = float(line[1])
         else:
             line = line.split(',')
             if not line[1].isalpha():
@@ -23,7 +23,14 @@ def generate_occupations():
     return occ_dict
 
 def random_occupation(occ_dict):
+    count = 0
     random_num = randint(0,99)
+    for key in occ_dict:
+        count += occ_dict[key]
+        if random_num < count:
+            return key
+
+print random_occupation(generate_occupations())
 
     
 
