5e2a80ee6fc0dd191c4f48ba923475acb0f9b9bb
Ryan Siu <rsiu@stuy.edu>
2017-09-15 14:28:36 -0400

    Tweak random algorithm

--- occupation.py
+++ occupation.py
@@ -1,6 +1,7 @@
-from random import randint
+import random
 
 FILENAME = 'occupations.csv'
+NUM_ITERATIONS = 1000000
 
 def generate_occupations():
     occ_dict = {}
@@ -24,13 +25,31 @@ def generate_occupations():
 
 def random_occupation(occ_dict):
     count = 0
-    random_num = randint(0,99)
+    random_num = random.random()*100
     for key in occ_dict:
         count += occ_dict[key]
         if random_num < count:
             return key
 
+def print_test_random(num):
+    counter = {}
+    occupations = generate_occupations()
+    for key in occupations:
+        counter[key] = 0
+
+    for i in range(0,num):
+        counter[random_occupation(occupations)] += 1
+
+    for key in counter:
+        #print key + ': ' + str(counter[key])
+        print key + ': ' + str(round(percent_error(occupations[key]*NUM_ITERATIONS/100, counter[key]), 2)) + '%'
+
+def percent_error(actual, measured):
+    return abs((actual-measured)/actual)*100
+
+# To test randomness of random selection
+#print 'Percent error of each occupations for ' + str(NUM_ITERATIONS) + ' ITERATIONS'
+#print_test_random(NUM_ITERATIONS)
+
+# To print a random occupation
 print random_occupation(generate_occupations())
-
-    
-
